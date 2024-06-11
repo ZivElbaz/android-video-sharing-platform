50a98bdfc06eace1d35106a069c040b840c14ffc
YuvalMaaravi <yuvalm1998@gmail.com>
2024-06-11 18:07:42 +0300

    handeling users

--- app/src/main/java/com/example/viewtube/managers/UsersManager.java
+++ app/src/main/java/com/example/viewtube/managers/UsersManager.java
@@ -1,17 +1,24 @@
 package com.example.viewtube.managers;
 
+import android.net.Uri;
+
+import com.example.viewtube.R;
 import com.example.viewtube.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class UsersManager {
     private static UsersManager instance;
     private List<User> users;
     private User currentUser;
+    private Uri profilePictureHard;
 
     private UsersManager() {
         users = new ArrayList<>();
+       profilePictureHard = Uri.parse("android.resource://com.example.viewtube/" + R.drawable.ic_profile);
+        users.add(new User("admin", "admin", "admin", "admin", profilePictureHard.toString() ));
     }
 
     public static synchronized UsersManager getInstance() {
